7eed3435608650368793478082730c99122fa0fb
prateek_hex <prateek.gupta@hexaviewtech.com>
2025-07-29 15:03:39 +0530

    added demo

---
+++ demo1.py
@@ -0,0 +1,5 @@
+def transform():
+    print("Transforming data...")
+
+if __name__ == "__main__":
+    transform()
---
+++ git demo-1.py
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 0,
+   "metadata": {
+    "application/vnd.databricks.v1+cell": {
+     "cellMetadata": {
+      "byteLimit": 2048000,
+      "rowLimit": 10000
+     },
+     "inputWidgets": {},
+     "nuid": "bd4a7a4a-7ce0-4add-b4a9-fca6bc438f40",
+     "showTitle": false,
+     "tableResultSettingsMap": {},
+     "title": ""
+    }
+   },
+   "outputs": [
+    {
+     "output_type": "stream",
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "transforming data....\n"
+     ]
+    }
+   ],
+   "source": [
+    "def transform_data_using_notebook():\n",
+    "    print(\"transforming data....\")\n",
+    "\n",
+    "\n",
+    "transform_data_using_notebook()"
+   ]
+  }
+ ],
+ "metadata": {
+  "application/vnd.databricks.v1+notebook": {
+   "computePreferences": null,
+   "dashboards": [],
+   "environmentMetadata": {
+    "base_environment": "",
+    "environment_version": "2"
+   },
+   "inputWidgetPreferences": null,
+   "language": "python",
+   "notebookMetadata": {
+    "pythonIndentUnit": 4
+   },
+   "notebookName": "git demo-1",
+   "widgets": {}
+  },
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 0
+}
